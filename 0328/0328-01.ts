30787e4d6e16bafb62ed67f4277142e14a2b78f4
CassHsu <cass.hsu@gmail.com>
2021-02-19 10:22:44 +0800

    Approach 1

---
+++ 0328/0328-01.ts
@@ -0,0 +1,17 @@
+function oddEvenList(head: ListNode | null): ListNode | null {
+    if (head === null) return null;
+    
+    let odd = head;
+    let even = odd.next;
+    let evenHead = even;
+    
+    while (even !== null && even.next !== null) {
+        odd.next = even.next;
+        odd = odd.next;
+        even.next = odd.next;
+        even = even.next;
+    }
+    
+    odd.next = evenHead;
+    return head;
+};
