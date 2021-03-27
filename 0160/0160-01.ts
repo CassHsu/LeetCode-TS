1cd7235952e9c69ea63fbe1a5767f2ef1248af80
CassHsu <cass.hsu@gmail.com>
2021-03-27 18:17:48 +0800

    Create 0160-01.ts

---
+++ 0160/0160-01.ts
@@ -0,0 +1,11 @@
+function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+    if (!headA || !headB) return null;
+    
+    let a = headA;
+    let b = headB;
+    while (a !== b) {
+        a = a? a.next: headB;
+        b = b? b.next: headA;
+    }
+    return a;
+};
