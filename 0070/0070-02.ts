80905edb385473ca039b48922af34e1b4b7e070b
CassHsu <cass.hsu@gmail.com>
2021-04-25 18:26:58 +0800

    Update 0070-02.ts

--- 0070/0070-02.ts
+++ 0070/0070-02.ts
@@ -1,10 +1,7 @@
 function climbStairs(n: number): number {
     if (n == 1) return 1;
     
-    const dp: number[] = [];
-    for (let i = 0; i <= n; i++) {
-        dp.push(0);
-    }
+    const dp = new Array(n+1).fill(0);
     dp[1] = 1;
     dp[2] = 2;
     
