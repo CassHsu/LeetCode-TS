6e6e347aeeb9b6b81b221b893b109c004cc08407
CassHsu <cass.hsu@gmail.com>
2021-07-04 18:03:57 +0800

    Approach 1: Recursive

---
+++ 0235/0235-01.ts
@@ -0,0 +1,11 @@
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	if (p.val > root.val && q.val > root.val) {
+        return lowestCommonAncestor(root.right, p, q);
+        
+    } else if (p.val < root.val && q.val < root.val) {
+        return lowestCommonAncestor(root.left, p, q);
+        
+    } else {
+        return root;
+    }
+};
