588b9f1b25183cb1e9b009f11fe42cc9e298cf1b
CassHsu <cass.hsu@gmail.com>
2021-07-28 22:07:40 +0800

    Update 0872-01.ts

--- 0872/0872-01.ts
+++ 0872/0872-01.ts
@@ -1,16 +1,16 @@
-var leafSimilar = function(root1, root2) {
+function leafSimilar(root1: TreeNode | null, root2: TreeNode | null): boolean {
     const dfs = (node) => {
         if (!node.left && !node.right) return node.val.toString();
         
         let ret = "";
         if (node.left) {
             const v = dfs(node.left);
-            ret = (ret != "") ? ret + "-" + v: v;
+            ret = (ret != "") ? ret + "-" + v : v;
         }
         
         if (node.right) {
             const v = dfs(node.right);
-            ret = (ret != "") ? ret + "-" + v: v;
+            ret = (ret != "") ? ret + "-" + v : v;
         }
         
         return ret;
