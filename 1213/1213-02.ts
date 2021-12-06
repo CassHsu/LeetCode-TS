a41346ba639e4186ceca061f0d6d50f6cc1723b2
CassHsu <cass.hsu@gmail.com>
2021-12-06 22:36:09 +0800

    Tree Pointer

---
+++ 1213/1213-02.ts
@@ -0,0 +1,24 @@
+function arraysIntersection(arr1: number[], arr2: number[], arr3: number[]): number[] {
+    const ans = [];
+    let p1 = 0, p2 = 0, p3 = 0;
+    
+    while (p1 < arr1.length && p2 < arr2.length && p3 < arr3.length) {
+        if (arr1[p1] === arr2[p2] && arr2[p2] === arr3[p3]) {
+            ans.push(arr1[p1]);
+            p1++;
+            p2++;
+            p3++;
+            
+        } else {
+            if (arr1[p1] < arr2[p2]) {
+                p1++;
+            } else if (arr2[p2] < arr3[p3]) {
+                p2++;
+            } else {
+                p3++;
+            }
+        }
+    }
+    
+    return ans;
+};
