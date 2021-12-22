9af36a3e578fd230a5ce21f15d3282e566fdf29a
CassHsu <cass.hsu@gmail.com>
2021-12-22 19:51:30 +0800

    DP

--- 1180/1180-01.ts
+++ 1180/1180-01.ts
@@ -1,12 +1,11 @@
 function countLetters(s: string): number {
     let count = 1
-    const dp = [1]
+    let dp = 1
     
     for (let i = 1; i < s.length; i++) {
-        if (s[i-1] === s[i]) dp.push(dp[i-1] + 1)
-        else dp.push(1)
-        
-        count += dp[i]
+        if (s[i-1] == s[i]) dp++
+        else dp = 1
+        count += dp
     }
     
     return count
