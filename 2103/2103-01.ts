95773ca3e9e8348619e27efe4383b4c6eed2d24d
CassHsu <cass.hsu@gmail.com>
2021-12-26 07:44:55 +0800

    Map

---
+++ 2103/2103-01.ts
@@ -0,0 +1,26 @@
+function countPoints(rings: string): number {
+    let count = 0
+    const m = new Map();
+    
+    for (let c = 0, r = 1; r < rings.length; c += 2, r += 2) {
+        if (!m.has(rings[r])) m.set(rings[r], rings[c])
+        else m.set(rings[r], m.get(rings[r]) + rings[c])
+    }
+    
+    for (const v of m.values()) {
+        let r = false, g = false, b = false
+        
+        for (const c of v) {
+            if (c === "R") r = true
+            if (c === "G") g = true
+            if (c === "B") b = true
+            
+            if (r && g && b) {
+                count++
+                break
+            }
+        }
+    }
+    
+    return count
+};
