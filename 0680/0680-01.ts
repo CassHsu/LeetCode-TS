d359813a243a05757ceb3e4f45bf25e3834e9fda
CassHsu <cass.hsu@gmail.com>
2022-04-03 09:53:28 +0800

    Two Pointers

---
+++ 0680/0680-01.ts
@@ -0,0 +1,22 @@
+function validPalindrome(s: string): boolean {
+    const isPalindrome = (s, i, j) => {
+        while (i < j) {
+            if (s[i] !== s[j]) return false
+            i++
+            j--
+        }
+        return true
+    };
+    
+    let l = 0
+    let r = s.length - 1
+    while (l < r) {
+        if (s[l] !== s[r]) {
+            return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1)
+        }
+        l++
+        r--
+    }
+    
+    return true
+};
