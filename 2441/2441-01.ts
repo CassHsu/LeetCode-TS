91d24248a534f23b30906ef95c22aac0e526406b
CassHsu <cass.hsu@gmail.com>
2022-11-04 12:17:42 +0800

    One Pass

--- 2441/2441-01.ts
+++ 2441/2441-01.ts
@@ -1,15 +1,14 @@
 function findMaxK(nums: number[]): number {
-    let max = -1
+    let res = -1
     const m = new Map()
     
     nums.forEach(n => {
         if (m.has(n)) m.set(n, m.get(n) + 1)
         else m.set(n, 1)
+        
+        if (m.has(-n)) res = Math.max(res, Math.abs(n))
     })
     
-    nums.forEach(n => {
-        if (n > 0 && m.has(-n) && n > max) max = n
-    })
     
-    return max
+    return res
 };
