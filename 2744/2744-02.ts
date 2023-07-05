36a644a66d21beee232523490ea67d93769bae2a
CassHsu <cass.hsu@gmail.com>
2023-07-05 22:04:40 +0800

    Update 2744-02.ts

--- 2744/2744-02.ts
+++ 2744/2744-02.ts
@@ -1,14 +1,14 @@
 function maximumNumberOfStringPairs(words: string[]): number {
     let count = 0
-    const pairs = new Set()
+    const n = words.length
 
-    words.forEach(w => {
-        const rw = w.split("").reverse().join("")
-        if (w != rw && !pairs.has(rw) && words.includes(rw)) {
-            count++
-            pairs.add(w)
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (words[i][0] == words[j][1] && words[i][1] == words[j][0]) {
+                count++
+            }
         }
-    })
+    }    
 
     return count
 };
