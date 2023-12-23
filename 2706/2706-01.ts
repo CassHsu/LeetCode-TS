0968244ee9600b57a223109fbae96e8d08cc2fd2
CassHsu <cass.hsu@gmail.com>
2023-12-23 22:27:24 +0800

    Update 2706-01.ts

--- 2706/2706-01.ts
+++ 2706/2706-01.ts
@@ -1,17 +1,17 @@
-var buyChoco = function(prices, money) {
-    let res = money;
-    let step = 0;
-    const size = prices.length;
-    prices.sort((a, b) => a - b);
+function buyChoco(prices: number[], money: number): number {
+    let res = money
+    let step = 0
+    const size = prices.length
+    prices.sort((a, b) => a - b)
 
     for (let i = 0; i < size; i++) {
-        const p = prices[i];
+        const p = prices[i]
         if (p <= res) {
-            res -= p;
-            step += 1;
+            res -= p
+            step += 1
 
-            if (step == 2) return res;
-        } else return money;
+            if (step == 2) return res
+        } else return money
     }
-    return money;
+    return money
 };
